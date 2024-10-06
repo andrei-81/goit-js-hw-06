8a0043ad00cfa2bc1e8d021b3dd4a01e57919de1
AndriHordiichuk <166404529+AndriHordiichuk@users.noreply.github.com>
2024-10-06 23:30:27 +0300

    commit

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+class Storage {
+    #items;
+
+    constructor(items) {
+        this.#items = items;
+    }
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(i => i != itemToRemove)
+    }
+}
+
+
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
